71f37d9ec1941272291243361df9ded6a53662b9
jackwardell <jackwardell@me.com>
2020-03-18 08:58:29 +0000

    adding more content

--- app.py
+++ app.py
@@ -9,15 +9,8 @@ toolbar = DebugToolbarExtension(app)
 
 
 class Description:
-    content = (
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
-        "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut"
-        "enim ad minim veniam, quis nostrud exercitation ullamco laboris "
-        "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor "
-        "in reprehenderit in voluptate velit esse cillum dolore eu fugiat "
-        "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt "
-        "in culpa qui officia deserunt mollit anim id est laborum."
-    )
+    def __init__(self, lines):
+        self.lines = lines
 
 
 class Job:
@@ -46,30 +39,112 @@ class CurriculumVitae:
     email_address = "jackwardell@me.com"
     phone_number = "+44 7868 118 271"
     job_title = "Data Engineer / Software Engineer / AI Engineer / Technologist"
+    nationality = "British National"
 
     # personal summary
-    summary = "I am an enthusiastic software developer who genuinely derives pleasure from learning and building."
+    summary = """I am an enthusiastic software developer who genuinely derives pleasure from 
+    learning and building. I am motivated by an innate curiosity and a desire to master my technologies.
+    I consider myself an engineer first """
 
     # job history
     jobs = [
-        Job("AI Engineer", "Shell", "Dec 2019", "Jun 2020", "London", Description),
-        Job("Data Engineer", "Shell", "Jun 2018", "Dec 2019", "London", Description),
+        Job(
+            "AI Engineer",
+            "Shell",
+            "Dec 2019",
+            "Jun 2020",
+            "London",
+            Description(
+                lines=[
+                    """A continuation of my previous role as a Data Engineer with a greater 
+                    focus on modeling and productionising data science work.""",
+                    "During this time I completed the Udacity course: Introduction to Machine Learning",
+                ]
+            ),
+        ),
+        Job(
+            "Data Engineer",
+            "Shell",
+            "Jun 2018",
+            "Dec 2019",
+            "London",
+            Description(
+                lines=[
+                    """On-site consultant at Shell working as a data engineer. 
+                    During this time, in a team, we delivered a tool for internal customers.
+                    Using ScrumBan and a lean startup approach, I interacted with customers 
+                    weekly to understand their pain and to build features they actually wanted.""",
+                    "Delivered:",
+                    [
+                        "A CI/CD pipeline",
+                        "Data ingestion from multiple sources and processing",
+                        "Deployed models to the cloud",
+                        "A website / tool frontend",
+                        "Migration from monolithic codebase to kubernetes",
+                    ],
+                    "Skills used:",
+                    [
+                        "Backend: Python, including Flask, SQLAlchemy, Pandas, Numpy, Scikit-learn",
+                        "CI: Git, Jenkins",
+                        "CD: Azure Web App, Kubernetes, VMs",
+                        "Databases: PostgreSQL, Redis, Azure Datalake Store",
+                        "Cloud: Azure",
+                        "Front End: HTML, CSS, Javascript, JQuery",
+                        "Agile: Lean Startup, Mutual Learning, Scrum, Kanban",
+                        "Other: Jira, Slack, ACT, RFT",
+                    ],
+                    "Internally awarded prizes within Shell:",
+                    [
+                        "Code Masters Award",
+                        "Downstream Directors Award",
+                        "Chemicals: Increasing profitability in our base business",
+                    ],
+                ]
+            ),
+        ),
         Job(
             "Big Data Engineer",
             "Kubrick Group",
             "Feb 2018",
             "Jun 2020",
             "London",
-            Description,
+            Description(
+                lines=[
+                    """For 4 months I was rigorously trained on - site in the
+                    discipline of big data engineering.In this bootcamp I
+                    learnt technical skills and soft skills for business.
+                    Including but not limited to:""",
+                    [
+                        "Python",
+                        "SQL (T-SQL, Microsoft SQL Server)",
+                        "NoSQL (MongoDB)",
+                        "Spark (Hadoop, Hive)",
+                        "Excel",
+                    ],
+                    """I was then placed at Shell on client site for 2 years where I am now.""",
+                ]
+            ),
         ),
     ]
 
     # education history
     education = [
-        Education("UCL", "2018", "London", Description, "MSci Chemistry"),
+        Education(
+            "UCL",
+            "2018",
+            "London",
+            Description(
+                lines=[
+                    """3rd Year dissertation on ‘Structural Defects in Metal–Organic Frameworks’ under Prof.Ben Slater""",
+                    """4th Year advanced chemical project on ‘Synthesis and Study of Exotic Magnets – Engineering New Quantum States’ under Prof.Andrew Wills""",
+                ]
+            ),
+            "MSci Chemistry",
+        ),
         Education("Abingdon School", "2013", "Oxford", Description),
     ]
 
+    # languages
     languages = ["English"]
 
 
